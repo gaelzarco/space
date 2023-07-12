e1041818f3658fb4f9b868fae7ddf23c848bf55e
gael <76548719+gaelzarco@users.noreply.github.com>
2023-07-11 17:36:40 -0700

    redis debug

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,18 @@
 import { Redis } from '@upstash/redis'
 
+function getDbCredentials() {
+  const dbUrl = process.env.DATABASE_URL
+  const dbToken = process.env.DATABASE_TOKEN
+  if (!dbUrl || !dbToken) {
+    throw new Error('DATABASE_URL and DATABASE_TOKEN must be set')
+  }
+  return {
+    dbUrl,
+    dbToken,
+  }
+}
+
 export const db = new Redis({
-  url: process.env.DATABASE_URL as string,
-  token: process.env.DATABASE_TOKEN as string,
+  url: getDbCredentials().dbUrl,
+  token: getDbCredentials().dbToken,
 })
