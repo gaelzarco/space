c3a50dc0e5400e20f724b5ab94d3fc45823b251c
gael <76548719+gaelzarco@users.noreply.github.com>
2023-07-12 17:13:04 -0700

    middleware cleanup

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,12 @@
+import { withAuth } from 'next-auth/middleware'
+
 export const config = {
   matcher: ['/dashboard'],
 }
 
-import { withAuth } from 'next-auth/middleware'
-
 export default withAuth(
   // `withAuth` augments your `Request` with the user's token.
-  function middleware(req) {
-    console.log(req.nextauth.token)
-  },
+  function middleware() {},
   {
     callbacks: {
       authorized: ({ token }) => token?.id !== null,
